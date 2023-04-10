8b54f36e8ab57db977ddfecc7230b634f6642fd8
alejandro-aires <128812491+alejandro-aires@users.noreply.github.com>
2023-04-10 11:18:25 -0300

    Add files via upload

---
+++ opencart/src/test/java/us/abstracta/jmeter/javadsl/sample/baseClass.java
@@ -0,0 +1,16 @@
+package us.abstracta.jmeter.javadsl.sample;
+
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
+import java.io.IOException;
+import java.time.Duration;
+import org.junit.jupiter.api.Test;
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+public class baseClass {
+
+    static String URL = "https://sucive.gub.uy";
+
+}
